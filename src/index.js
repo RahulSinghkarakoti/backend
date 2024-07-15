8df87696c10915495658298eee2df8e693b10625
rahulsinghkarakoti <rahulsinghkarakoti28@gmail.com>
2024-07-15 13:45:12 +0530

    added mongodb connection

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constant.js";
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+})
+
+connectDB();
+
+/*
+import express from "express";
+const app=express()
+(async()=>{
+    try{
+  
+       await mongoose.connect(`${process.env.MONOGO_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("error:",error)
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`APP is listening at port: ${process.env.PORT}`)
+        })
+    }catch(error){
+console.log("ERROR:",error)
+    }
+})()
+
+*/
