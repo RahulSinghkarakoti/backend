06d171f17fa42205904e6441275f6b71931d6f18
rahulsinghkarakoti <rahulsinghkarakoti28@gmail.com>
2024-07-20 14:47:13 +0530

    added routes and mongo pipelines

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,17 +1,17 @@
 import mongoose, { mongo, Schema } from "mongoose"
 
 const subscriptionSchema=new mongoose.Schema(
-    {
-       suscriber:{
+    { 
+       suscriber:{  //followers
         type:Schema.Types.ObjectId,//one who is subscribing
         ref:'User' 
        },
-       channel:{
+       channel:{ //following
         type:Schema.Types.ObjectId,//one to who is subscriber is subscribing (a user subscribing a user)
         ref:'User' 
        }
-       
+
     }
     ,{timestamps:true})
 
-export const Subscription = mongoose.model("Sunscription",subscriptionSchema)
+export const Subscription = mongoose.model("Subscription",subscriptionSchema)
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,31 +1,58 @@
-import { Router } from "express"
-import { upload } from "../middlewares/multer.middleware.js"
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js"
-import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { Router } from "express";
+import { upload } from "../middlewares/multer.middleware.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router=Router()
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([    //middleware
-        {
-            name: "avatar",
-            maxCount:1
-        },
-        {
-            name: "coverImage",
-            maxCount:1
-        }
-]),
+  upload.fields([
+    //middleware
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
 
-    registerUser  
-)
+  registerUser
+);
 
 // router.route("/login").post(loginUser)
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 //secured route
-router.route("/logout").post(verifyJWT,  logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
-export default router
+router
+    .route("/avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateAvatar);
+
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateCoverImage);
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
+
+export default router;
