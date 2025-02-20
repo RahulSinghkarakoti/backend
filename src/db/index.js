836bb66ff32b97526501c42b6d051a280ea5c170
rahul karakoti <rahulsinghkarakoti28@gmail.com>
2025-02-20 16:14:16 +0530

    repaire some run issue

--- src/db/index.js
+++ src/db/index.js
@@ -3,6 +3,7 @@ import { DB_NAME } from "../constant.js";
 
 const connectDB=async ()=>{
     try {
+        console.log(process.env.MONOGO_URI)
        const connectionInstance = await mongoose.connect(`${process.env.MONOGO_URI}/${DB_NAME}`)
        console.log(`\n MongoDB connected !! DB HOST :${connectionInstance.connection.host}`)
 
